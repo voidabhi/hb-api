39c7f730b002d2696279711f8384c6f4266ea3f6
Abhijeet Mohan <void.aby@gmail.com>
2016-01-12 21:25:12 +0530

    add lint task in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,14 @@
 
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint');
+
+
+gulp.task('default', function () {
+    runsequence('lint');
+});
+
+gulp.task('lint', function () {
+    gulp.src('**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
