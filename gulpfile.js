c1377f5986785148a503b4eef2fa9e669ff12c6c
Abhijeet Mohan <void.aby@gmail.com>
2016-01-23 22:31:01 +0530

    add test task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 
 var gulp = require('gulp'),
     jscs = require('gulp-jscs'),
+    tape = require('tape'),
+    spec = require('tap-spec'),
     jshint = require('gulp-jshint');
 
 
@@ -17,5 +19,14 @@ gulp.task('lint', function () {
 gulp.task('jscs', function () {
     return gulp.src('**/*.js')
         .pipe(jscs());
+});
+
+gulp.task('test', function() {
+    return gulp.src(paths.serverTests, {
+            read: false
+        })
+        .pipe(tape.createStream())
+        .pipe(spec())
+        .pipe(process.stdout);
 });
 
